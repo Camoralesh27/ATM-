5d64d0f26a055deb73037e206be3e4cec059d1bb
Cristhian Morales <camoralesh27@gmail.com>
2022-03-31 23:33:39 -0700

    funcion registro funcional, anexa personas al arreglo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,59 @@
+
 let cuentas=[
-    {nombre: "Mali", saldo: 200 , password: "helloword"},
-    {nombre: "Gera", saldo: 290 , password: "123t"},
-    {nombre: "Maui", saldo: 67 , password: "123"},
+    {nombre: "Mali", 
+    usuario:"Mali", 
+    contraseña: "helloword", 
+    saldo: 200},
+
+    {nombre: "Gera", 
+    usuario: "Gera", 
+    contraseña: "123t", 
+    saldo: 290},
+
+    {nombre: "Maui", 
+    usuario: "Maui", 
+    contraseña: "123", 
+    saldo: 67},
 ]
+
+/* Función para meter usuarios nuevos al arreglo */
+function registro(event){
+    event.preventDefault();
+    let name = document.getElementById("inputNombre").value;
+    let user = document.getElementById("inputUsuario").value;
+    let password = document.getElementById("inputContraseña").value;
+    let amount = document.getElementById("inputSaldo").value;
+
+    //para ver el usuario que agrege
+    console.log(name,user,password,amount);
+    
+    //cambiar de string a numero el saldo
+    amount = parseFloat(amount);
+
+    let usuar = {
+        nombre: name,
+        usuario: user,
+        contraseña: password,
+        saldo: amount
+    }
+
+    //imprime el objeto
+    console.log(usuar);
+    
+    //sube el objeto al arreglo y tenemos un nuevo usuario
+    cuentas.push(usuar);
+}
+
+
+/* Función para login al banco */
+/* 
+function login(){
+let username = document.getElementById("inputNombre").value;
+let password1 = document.getElementById("inputContra1").value;
+
+if(username === cuentas.nombre && password1 === cuentas.contraseña )
+    window.location.href = "index.html";
+
+    console.log()
+}
+ */
